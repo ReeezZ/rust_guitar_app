204bd7dcbca47578e9233866a86e79f78b4b5ee4
Mario Backes <mba@thepeaklab.com>
2025-02-16 14:53:23 +0100

    fretboard v2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod my_app;
+mod fretboard_v2;
 
 use leptos::mount::mount_to_body;
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(my_app::App);
+    // mount_to_body(my_app::App);
+    mount_to_body(fretboard_v2::FretboardV2);
 }
