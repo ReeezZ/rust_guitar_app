b909412f584ca09ccc087c61649901a9125173e8
Mario Backes <mba@thepeaklab.com>
2025-02-16 21:17:26 +0100

    change format to 2 spaces indentation

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,16 @@
-use leptos::{prelude::{ClassAttribute, ElementChild}, *};
+use leptos::{
+  prelude::{ClassAttribute, ElementChild},
+  *,
+};
 
 use crate::fretboard::Fretboard;
 
 #[component]
 pub fn App() -> impl IntoView {
-    view! {
-        <div class="bg-gray-900">
-            <h1>"Gitarren Griffbrett"</h1>
-            <Fretboard num_frets=15 num_strings=6 />
-        </div>
-    }
+  view! {
+    <div class="bg-gray-900">
+      <h1>"Gitarren Griffbrett"</h1>
+      <Fretboard num_frets=15 num_strings=6 />
+    </div>
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod fretboard;
 use leptos::mount::mount_to_body;
 
 fn main() {
-    console_error_panic_hook::set_once();
-    // mount_to_body(my_app::App);
-    mount_to_body(app::App);
+  console_error_panic_hook::set_once();
+  // mount_to_body(my_app::App);
+  mount_to_body(app::App);
 }
