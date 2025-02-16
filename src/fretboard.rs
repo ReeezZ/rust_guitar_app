16c14a84218325f9831709093d281a87a1f32a78
Mario <mariobackes@outlook.de>
2025-02-16 16:21:07 +0100

    Set up tailwind

--- src/fretboard.rs
+++ src/fretboard.rs
@@ -6,11 +6,11 @@ pub fn Fretboard(
     #[prop(default = 15)] num_frets: u8,
 ) -> impl IntoView {
     view! {
-        <div class="fretboard">
+        <div class="fretboard bg-100">
             {(0..num_strings)
                 .map(|_| {
                     view! {
-                        <div class="note">
+                        <div class="py-2 px-3 m-1 text-white bg-blue-700 rounded border-l-2 border-b-4 border-blue-800 shadow-lg">
                             {(0..num_frets)
                                 .map({ |_| view! { <div class="note-string">LUL</div> } })
                                 .collect::<Vec<_>>()}
