72956fdcaea7a253ab4042bfab67f0f792fae27f
Mario <mariobackes@outlook.de>
2025-02-16 09:48:15 +0100

    try out dynamic css

--- src/my_app.rs
+++ src/my_app.rs
@@ -5,10 +5,26 @@ pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <button on:click=move |_| *set_count.write() += 1>"Click me: " {count}</button>
-        <p>"Double count: " {move || count.get() * 2}</p>
+        <button
+            on:click=move |_| {
+                *set_count.write() += 10;
+            }
+            // set the `style` attribute
+            style="position: absolute"
+            // and toggle individual CSS properties with `style:`
+            style:left=move || format!("{}px", count.get() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
+            style:max-width="400px"
+            // Set a CSS variable for stylesheet use
+            style=("--columns", move || count.get().to_string())
+        >
+            "Click to Move"
+        </button>
+        <progress
+    max="50"
+    // signals are functions, so `value=count` and `value=move || count.get()`
+    // are interchangeable.
+    value=count
+/>
     }
 }
-
-
-
