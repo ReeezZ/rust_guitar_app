0a11cbe087fe09256ea959e477010de49e9fde7c
Henning Piehler <henning-piehler@web.de>
2025-02-17 13:38:40 +0100

    added string 'texture'

--- src/fretboard.rs
+++ src/fretboard.rs
@@ -11,22 +11,22 @@ pub fn Fretboard(
   #[prop(default = 15)] num_frets: u8,
 ) -> impl IntoView {
   view! {
-    <div class="p-6 pt-6 pb-2 mx-16 mt-12 bg-amber-500 bg-opacity-70 rounded">
-      <div class="bg-[#be975b] bg-fretboard">
+    <div class="p-6 mx-16 mt-12 bg-amber-500 bg-opacity-70 rounded">
+      <div class="bg-[#be975b] bg-fretboard relative">
         {(0..num_strings)
           .map(|string_no| {
             let string_strength = 2.0 + 0.65 * string_no as f64;
             view! {
               <div class="flex relative justify-start items-center w-full">
                 // Nut (Thick first fret)
-                <div class="flex justify-center items-center w-16 h-12 font-bold text-center text-white bg-gray-800 border-r-8 border-slate-900">
+                <div class="flex justify-center items-center w-16 h-12 font-bold text-center text-white bg-[#494949] border-r-8 border-slate-900">
                   <span>{string_no}- 0</span>
                 </div>
                 // Fretboard Section (Holds both string + frets)
                 <div class="flex relative grow">
                   // String
                   <div
-                    class="absolute right-0 left-0 top-1/2 z-20 -translate-y-1/2 bg-slate-300"
+                    class="absolute right-0 left-0 top-1/2 z-20 -translate-y-1/2 bg-[repeating-linear-gradient(45deg,_#dddddd,_#555555_2px,_#333333_2px)]"
                     style:height=move || format!("{}px", string_strength)
                   ></div>
 
@@ -34,7 +34,7 @@ pub fn Fretboard(
                   {(1..=num_frets)
                     .map(|fret_no| {
                       view! {
-                        <div class="relative flex grow shrink w-full justify-center items-center h-12 text-center bg-transparent border-r-4 [border-image:linear-gradient(0.25turn,#7c7c7c,#bbbbbb,#7c7c7c)_1_60%]">
+                        <div class="relative flex grow w-full justify-center h-12 items-center text-center bg-transparent border-r-4 [border-image:linear-gradient(0.25turn,#7c7c7c,#cccccc,#7c7c7c)_1_60%]">
                           <span class="z-20">{string_no}- {fret_no}</span>
                         </div>
                       }
@@ -46,8 +46,8 @@ pub fn Fretboard(
           })
           .collect::<Vec<_>>()}
       // Fret markers row (positioned below the frets)
-      </div>
       <FretboardMarker num_frets=num_frets />
+      </div>
     </div>
   }
 }
@@ -55,7 +55,7 @@ pub fn Fretboard(
 #[component]
 pub fn FretboardMarker(#[prop()] num_frets: u8) -> impl IntoView {
   view! {
-    <div class="flex justify-start w-full">
+    <div class="absolute top-[48%] flex justify-start w-full">
       // Empty space for the nut
       <div class="flex w-16 h-4 border-transparent border-r-[8px] "></div>
       {(1..=num_frets)
@@ -64,19 +64,21 @@ pub fn FretboardMarker(#[prop()] num_frets: u8) -> impl IntoView {
           let is_double = fret_no % 12 == 0;
 
           view! {
-            <div class="flex relative justify-center flex-grow items-center h-4 border-r border-black">
+            <div class="flex relative justify-center flex-grow items-center h-4">
               {move || {
                 if is_double {
                   EitherOf3::A(
                     view! {
                       <>
-                        <div class="absolute left-1/4 w-2 h-2 bg-black rounded-full"></div>
-                        <div class="absolute right-1/4 w-2 h-2 bg-black rounded-full"></div>
+                        <div class="absolute translate-y-12 w-4 h-4 border border-[#a1a09f] bg-[linear-gradient(152deg,_#fff_20%,_#a8a499_100%,_#a8a499)] rounded-full"></div>
+                        <div class="absolute -translate-y-12 w-4 h-4 border border-[#a1a09f] bg-[linear-gradient(152deg,_#fff_20%,_#a8a499_100%,_#a8a499)] rounded-full"></div>
                       </>
                     },
                   )
                 } else if has_marker {
-                  EitherOf3::B(view! { <div class="absolute w-2 h-2 bg-black rounded-full"></div> })
+                  EitherOf3::B(view! { 
+                    <div class="absolute w-4 h-4 border border-[#a1a09f] bg-[linear-gradient(152deg,_#fff_20%,_#a8a499_100%,_#a8a499)] rounded-full"></div>
+                   })
                 } else {
                   EitherOf3::C(view! { <></> })
                 }
