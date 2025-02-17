bb26a57903503384723a8a81439360a6e8c8d219
Henning Piehler <henning-piehler@web.de>
2025-02-17 01:45:01 +0100

    added lib and note enum

---
+++ src/note_enum.rs
@@ -0,0 +1,65 @@
+#![allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Note {
+  C,
+  CisOrDes,
+  D,
+  DisOrEs,
+  E,
+  F,
+  FisOrGes,
+  G,
+  GisOrAs,
+  A,
+  AisOrB,
+  H,
+}
+
+impl Note {
+  fn iterator() -> impl Iterator<Item = Note> {
+    [Note::C, Note::CisOrDes, Note::D].into_iter()
+  }
+
+  fn as_str(&self) -> &'static str {
+    match self {
+      Note::C => "C",
+      Note::CisOrDes => "C♯/D♭",
+      Note::D => "D",
+      Note::DisOrEs => "D♯/E♭",
+      Note::E => "E",
+      Note::F => "F",
+      Note::FisOrGes => "F♯/G♭",
+      Note::G => "G",
+      Note::GisOrAs => "G♯/A♭",
+      Note::A => "A",
+      Note::AisOrB => "A♯/B",
+      Note::H => "H",
+    }
+  }
+
+  fn get_all() -> Vec<Note> {
+    vec![
+      Note::C,
+      Note::CisOrDes,
+      Note::D,
+      Note::DisOrEs,
+      Note::E,
+      Note::F,
+      Note::FisOrGes,
+      Note::G,
+      Note::GisOrAs,
+      Note::A,
+      Note::AisOrB,
+      Note::H,
+    ]
+  }
+
+  fn get_notes_from(start: Note, length: usize) -> Vec<Note> {
+    let all_notes = Self::get_all();
+    let start_index = all_notes.iter().position(|&note| note == start).unwrap();
+
+    (0..length)
+      .map(|i| all_notes[(start_index + i) % 12])
+      .collect()
+  }
+}
