1448ca529f6817e44d038d2c9ebfe0a144866a48
Henning Piehler <henning-piehler@web.de>
2025-02-18 10:38:41 +0100

    styleing cleanup/ add fretboard shadow

--- src/fretboard.rs
+++ src/fretboard.rs
@@ -11,55 +11,60 @@ pub fn Fretboard(
   #[prop(default = 15)] num_frets: u8,
 ) -> impl IntoView {
   view! {
-    <div class="relative py-10 px-20 w-screen bg-cyan-700 rounded">
-      <div class="relative flex-col trapezoid grow bg-[#be975b] bg-fretboard">
-        {(0..num_strings)
-          .map(|string_no| {
-            let string_strength = 2.0 + 0.65 * string_no as f64;
-            view! {
-              <div class="flex relative justify-start items-center w-full string">
-                // Nut (Thick first fret)
-                // background: linear-gradient(0.25turn, #c9c9c9, #ffffff, #c9c9c9);
-                <div class="justify-center items-center z-30 w-14 drop-shadow-[0_2px_2px_rgba(0,0,0,1)] font-bold text-center text-white border-r-8 border-transparent">
-                  <span>{string_no}-0</span>
-                </div>
-                // Fretboard Section (Holds both string + frets)
-                <div class="flex relative grow">
-                  // String
-                  <div
-                    class="absolute drop-shadow-[0_2px_2px_rgba(0,0,0,0.6)] right-0 -left-20 top-1/2 z-20 -translate-y-1/2 bg-[repeating-linear-gradient(45deg,_#dddddd,_#555555_2px,_#333333_2px)]"
-                    style:height=move || format!("{}px", string_strength)
-                  ></div>
+    <div class="relative py-10 px-14 w-screen bg-cyan-700 rounded">
+      <div class="trapezoid-shadow">
+        <div class="relative flex-col trapezoid grow bg-[#be975b] bg-fretboard">
+          {(0..num_strings)
+            .map(|string_no| {
+              let string_strength = 2.0 + 0.5 * string_no as f64;
+              view! {
+                <div class="flex relative justify-start items-center w-full tilt">
+                  // Nut (Thick first fret)
+                  <div class="relative z-30 justify-center items-center w-8 h-6 border-r-8 border-transparent">
+                    <span class="absolute w-12 font-bold text-center text-white cursor-pointer drop-shadow-[0_2px_2px_rgba(0,0,0,1)]">
+                      {string_no}-0
+                    </span>
+                  </div>
+                  // Fretboard Section (Holds both string + frets)
+                  <div class="flex relative grow">
+                    // String
+                    <div
+                      class="absolute right-0 -left-60 top-1/2 z-20 -translate-y-1/2 drop-shadow-[0_2px_2px_rgba(0,0,0,0.6)] bg-[repeating-linear-gradient(45deg,_#dddddd,_#555555_2px,_#333333_2px)]"
+                      style:height=move || format!("{}px", string_strength)
+                    ></div>
 
-                  // Frets
-                  {(1..=num_frets)
-                    .map(|fret_no| {
-                      view! {
-                        // bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]
-                        <div class="flex relative justify-center items-center w-full h-12 text-center bg-transparent grow fretbar-container">
-                          <span class="drop-shadow-[0_2px_2px_rgba(0,0,0,1)] font-bold text-center text-white z-20 cursor-pointer">{string_no}- {fret_no}</span>
-                        // <div class="absolute w-1 h-14 left-1 fretbar border-[1px] border-black"></div>
-                        </div>
-                      }
-                    })
-                    .collect::<Vec<_>>()}
+                    // Frets
+                    {(1..=num_frets)
+                      .map(|fret_no| {
+                        view! {
+                          // bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]
+                          <div class="flex relative justify-center items-center w-full h-12 text-center bg-transparent grow fretbar-container">
+                            <span class="z-20 font-bold text-center text-white cursor-pointer drop-shadow-[0_2px_2px_rgba(0,0,0,1)]">
+                              {string_no}- {fret_no}
+                            </span>
+                          // <div class="absolute w-1 h-14 left-1 fretbar border-[1px] border-black"></div>
+                          </div>
+                        }
+                      })
+                      .collect::<Vec<_>>()}
+                  </div>
                 </div>
-              </div>
-            }
-          })
-          .collect::<Vec<_>>()} // Fret markers row (positioned below the frets)
-        <FretboardMarker num_frets=num_frets />
+              }
+            })
+            .collect::<Vec<_>>()} // Fret markers row (positioned below the frets)
+          <FretboardDetails num_frets=num_frets />
+        </div>
       </div>
     </div>
   }
 }
 
 #[component]
-pub fn FretboardMarker(#[prop()] num_frets: u8) -> impl IntoView {
+pub fn FretboardDetails(#[prop()] num_frets: u8) -> impl IntoView {
   view! {
     <div class="flex absolute justify-start w-full top-[48%]">
       // Empty space for the nut
-      <div class="flex relative -top-32 w-14 h-80 -z-10 bg-[linear-gradient(90deg,_#000_20%,_#333333_100%,_#a8a499)] border-r-[8px] [border-image:linear-gradient(0.25turn,#aaaaaa,#ffffff,#aaaaaa)_1_100%]"></div>
+      <div class="flex relative -top-32 w-8 h-80 -z-10 bg-[linear-gradient(90deg,_#000_20%,_#333333_100%,_#a8a499)] border-r-[8px] [border-image:linear-gradient(0.25turn,#aaaaaa,#ffffff,#aaaaaa)_1_100%]"></div>
       {(1..=num_frets)
         .map(|fret_no| {
           let has_marker = [3, 5, 7, 9, 15, 17, 19, 21].contains(&(fret_no % 12));
@@ -74,7 +79,7 @@ pub fn FretboardMarker(#[prop()] num_frets: u8) -> impl IntoView {
                       <>
                         <div class="absolute w-4 h-4 rounded-full border translate-y-12 border-[#a1a09f] bg-[linear-gradient(152deg,_#fff_20%,_#a8a499_100%,_#a8a499)]"></div>
                         <div class="absolute w-4 h-4 rounded-full border -translate-y-12 border-[#a1a09f] bg-[linear-gradient(152deg,_#fff_20%,_#a8a499_100%,_#a8a499)]"></div>
-                        <div class="absolute -z-10 left-full w-1 h-80 bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]"></div>
+                        <div class="absolute left-full w-1 h-80 -z-10 bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]"></div>
                       </>
                     },
                   )
@@ -82,14 +87,14 @@ pub fn FretboardMarker(#[prop()] num_frets: u8) -> impl IntoView {
                   EitherOf3::B(
                     view! {
                       <div class="absolute w-4 h-4 rounded-full border border-[#a1a09f] bg-[linear-gradient(152deg,_#fff_20%,_#a8a499_100%,_#a8a499)]"></div>
-                      <div class="absolute -z-10 left-full w-1 h-80 bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]"></div>
+                      <div class="absolute left-full w-1 h-80 -z-10 bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]"></div>
                     },
                   )
                 } else {
                   EitherOf3::C(
                     view! {
                       <>
-                        <div class="absolute -z-10 left-full w-1 h-80 bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]"></div>
+                        <div class="absolute left-full w-1 h-80 -z-10 bg-[linear-gradient(90deg,_#bbbbbb_40%,_#444433_100%,_#48a499)]"></div>
                       </>
                     },
                   )
