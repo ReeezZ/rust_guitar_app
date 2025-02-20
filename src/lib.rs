f45a3dc14e8b72cd7d847811a2f346abbd46d0e5
Henning Piehler <henning-piehler@web.de>
2025-02-20 23:29:45 +0100

    added meta context to website/ little cargo clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use leptos::{prelude::ElementChild, *};
+use leptos::{prelude::ElementChild, prelude::*};
+use leptos_meta::*;
+use leptos_router::{components::*, path};
 
 mod music;
 mod components;
@@ -7,7 +9,18 @@ use crate::components::fretboard::Fretboard;
 
 #[component]
 pub fn App() -> impl IntoView {
+
+  provide_meta_context();
+
+
   view! {
+    <Html attr:lang="en" attr:dir="ltr" />
+    <Title text="♫ Rust Guitar App ♫" />
+    
+    <Meta charset="UTF-8" />
+    <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    
     <div>
       <h1 class="py-6 text-6xl font-bold text-center text-cyan-700">"Gitarren Griffbrett"</h1>
       <Fretboard num_frets=24 num_strings=6 />
