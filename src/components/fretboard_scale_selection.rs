fb7d50ee2c4584b5469cb93082e766e6af1f66f1
Mario Backes <mba@thepeaklab.com>
2025-02-25 17:59:05 +0100

    Get passing of function pointers as RwSignal working

--- src/components/fretboard_scale_selection.rs
+++ src/components/fretboard_scale_selection.rs
@@ -12,16 +12,13 @@ fn note_to_string(note: Note) -> String {
 
 #[component]
 pub fn FretboardScaleSelection() -> impl IntoView {
-  let (is_note_visible_signal, set_is_note_visible_signal) = signal(is_note_visible);
-  let (note_to_string_signal, set_note_to_string_signal) = signal(note_to_string);
+  let (is_note_visible_signal, set_is_note_visible_signal) =
+    signal::<fn(Note) -> bool>(is_note_visible);
+  let (note_to_string_signal, set_note_to_string_signal) =
+    signal::<fn(Note) -> String>(note_to_string);
   view! {
     <div class="">
-      <Fretboard
-        num_frets=24
-        num_strings=6
-        is_note_visible_signal=is_note_visible_signal
-        note_to_string_signal
-      />
+      <Fretboard num_frets=24 num_strings=6 is_note_visible_signal note_to_string_signal />
     </div>
   }
 }
