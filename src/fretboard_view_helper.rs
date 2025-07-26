2780243f392bfd2cffc324fa30144bdce0560cf6
Mario B <mba@thepeaklab.com>
2025-07-26 13:38:53 +0000

    translate comments

--- src/fretboard_view_helper.rs
+++ src/fretboard_view_helper.rs
@@ -3,10 +3,10 @@
 /// For example, if your `viewBox` is 800 units wide and you want to display 24 frets,
 /// then the `scale_length` must be scaled accordingly so that it fits the width of your SVG.
 pub fn calculate_fret_positions(scale_length: f64, num_frets: u8) -> Vec<f64> {
-  let mut positions = Vec::with_capacity(num_frets as usize + 1); // +1 für Sattel
+  let mut positions = Vec::with_capacity(num_frets as usize + 1); // +1 for saddle
   let twelfth_root_of_2 = 2.0_f64.powf(1.0 / 12.0);
 
-  // Position des Sattels (Bund 0) ist 0
+  // Position of saddle (fret 0) is 0
   positions.push(0.0);
 
   for n in 1..=num_frets {
