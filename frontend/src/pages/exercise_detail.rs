8b8d8d3e8e68d6c90cc5301d290ee4390e7ccfd5
Mario B <mba@thepeaklab.com>
2025-08-31 15:51:03 +0200

    exercise_detail: pass the exercise id signal properly

--- frontend/src/pages/exercise_detail.rs
+++ frontend/src/pages/exercise_detail.rs
@@ -5,7 +5,7 @@ use leptos_router::hooks::use_params_map;
 #[component]
 pub fn ExerciseDetailPage() -> impl IntoView {
   let params = use_params_map();
-  let exercise_id = move || params.read_untracked().get("id").unwrap_or_default();
+  let exercise_id = Signal::derive(move || params.read().get("id").unwrap_or_default());
 
-  view! { <ExerciseDetail exercise_id=exercise_id() /> }
+  view! { <ExerciseDetail exercise_id /> }
 }
