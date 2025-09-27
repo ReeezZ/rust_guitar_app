0a9b47eb0d1b89b570c28908a86a0c339592d749
Mario B <mba@thepeaklab.com>
2025-09-27 22:57:23 +0200

    navbar: cleanup remove commented out code, style tweaks

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,10 +1,6 @@
-use codee::string::FromToStringCodec;
 use leptos::prelude::*;
 use leptos_router::hooks::use_location;
-use leptos_use::{
-  storage::{use_local_storage_with_options, use_session_storage, UseStorageOptions},
-  use_color_mode, ColorMode, UseColorModeReturn,
-};
+use leptos_use::{use_color_mode, ColorMode, UseColorModeReturn};
 
 use crate::components::ui::Toggle;
 
@@ -14,7 +10,7 @@ pub fn NavbarLinks() -> impl IntoView {
   let pathname = move || location.pathname.get();
 
   view! {
-    <div class="flex flex-wrap flex-1 gap-y-1 gap-x-2 items-center min-w-0">
+    <div class="flex flex-wrap flex-1 items-center min-w-0">
       <NavbarElement path="/" is_active=Signal::derive(move || pathname() == "/") text="Home" />
       <NavbarElement
         path="/exercises"
@@ -69,28 +65,12 @@ pub fn ThemeToggle() -> impl IntoView {
       unchecked_text="Light"
     />
   }
-
-  // view! {
-  //   <label class="inline-flex items-center cursor-pointer">
-  //     <input
-  //       type="checkbox"
-  //       class="sr-only peer"
-  //       checked=move || is_dark.get()
-  //       on:input=move |_| set_is_dark.set(!is_dark.get())
-  //     />
-  //
-  //     <div class="relative w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700 dark:border-gray-600 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600 dark:peer-focus:ring-blue-800 dark:peer-checked:bg-blue-600"></div>
-  //     <span class="text-sm font-medium text-gray-900 dark:text-gray-300 ms-3">
-  //       {move || if is_dark.get() { "Dark" } else { "Light" }}
-  //     </span>
-  //   </label>
-  // }
 }
 
 #[component]
 pub fn Navbar() -> impl IntoView {
   view! {
-    <div class="flex z-50 justify-between items-center p-4 grow h-fit">
+    <div class="flex z-50 justify-between items-center px-4 mb-2 grow h-fit">
       <NavbarLinks />
       <div class="pl-4 shrink-0">
         <ThemeToggle />
@@ -113,10 +93,7 @@ pub fn NavbarElement(
     >
 
       <a href=path>
-        <span>
-
-          {text}
-        </span>
+        <span>{text}</span>
       </a>
 
     </div>
